9693b0a2eb567b9d4a5be290d584b855aeee7a49
leesemin <leesemin222@gmail.com>
2023-04-21 02:29:10 +0900

    230420 과제 (exampleOne이 작동 안함)

---
+++ test.js
@@ -0,0 +1,30 @@
+// let a;
+// a = true;
+
+// console.log(a);
+
+// const b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// function c(b) {
+//   if (b.length % 2 === 1) {
+//     return new Error("에럽니다 알아서 처리하세요 짝수가 아니므니다");
+//   }
+// }
+// console.log(c(b));
+
+// function d(a, b, c) {
+//   return a + b + c;
+// }
+
+// const e = d(1, 2);
+// if (e === NaN) {
+//   e.c(3);
+// }
+// console.log(e);
+
+function a(ay) {
+  if (error) {
+    console.error("에러났당께로", error);
+  }
+}
+const b = new Error("dp");
+a(b);
