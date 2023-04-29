a43388d8a911d1f8b579c6146603335fbef38153
leesemin <leesemin222@gmail.com>
2023-04-29 18:59:55 +0900

    과제 종료

--- 230427-exam-9.js
+++ 230427-exam-9.js
@@ -1,36 +1,12 @@
 import inquirer from "inquirer";
 
-async function oddElevator(floorNumber) {
+function oddElevator(floorNumber) {
   if (floorNumber !== -1 && floorNumber !== 10) {
     if (floorNumber % 2 !== 1) {
       console.log(`해당 엘리베이터로는 갈 수 없는 층입니다.`);
     } else {
       console.log("문을 열겠습니다.");
-      await setTimeout(() => {
-        console.log(`${floorNumber}층에 도착하였습니다.`);
-      }, 1000);
       setTimeout(() => {
-        console.log("문을 닫습니다.");
-      }, 1000);
-    }
-  } else {
-    console.log("문을 열겠습니다.");
-    await setTimeout(() => {
-      console.log(`${floorNumber}층에 도착하였습니다.`);
-    }, 1000);
-    setTimeout(() => {
-      console.log("문을 닫습니다.");
-    }, 1000);
-  }
-}
-
-async function evenElevator(floorNumber) {
-  if (floorNumber !== -1 && floorNumber !== 1) {
-    if (floorNumber % 2 !== 0) {
-      console.log(`해당 엘리베이터로는 갈 수 없는 층입니다.`);
-    } else {
-      console.log("문을 열겠습니다.");
-      await setTimeout(() => {
         console.log(`${floorNumber}층에 도착하였습니다.`);
       }, 1000);
       setTimeout(() => {
@@ -39,7 +15,7 @@ async function evenElevator(floorNumber) {
     }
   } else {
     console.log("문을 열겠습니다.");
-    await setTimeout(() => {
+    setTimeout(() => {
       console.log(`${floorNumber}층에 도착하였습니다.`);
     }, 1000);
     setTimeout(() => {
@@ -48,5 +24,56 @@ async function evenElevator(floorNumber) {
   }
 }
 
-console.log(oddElevator(8));
-console.log(evenElevator(3));
+function evenElevator(floorNumber) {
+  if (floorNumber !== -1 && floorNumber !== 1) {
+    if (floorNumber % 2 !== 0) {
+      console.log(`해당 엘리베이터로는 갈 수 없는 층입니다.`);
+    } else {
+      console.log("문을 열겠습니다.");
+      setTimeout(() => {
+        console.log(`${floorNumber}층에 도착하였습니다.`);
+      }, 1000);
+      setTimeout(() => {
+        console.log("문을 닫습니다.");
+      }, 2000);
+    }
+  } else {
+    console.log("문을 열겠습니다.");
+    setTimeout(() => {
+      console.log(`${floorNumber}층에 도착하였습니다.`);
+    }, 1000);
+    setTimeout(() => {
+      console.log("문을 닫습니다.");
+    }, 2000);
+  }
+}
+
+// console.log(oddElevator(8));
+// console.log(evenElevator(3));
+
+const promptReq = [
+  {
+    type: "list",
+    name: "elevator",
+    message: "어떤 엘리베이터를 탑승하시겠습니까?",
+    choices: ["oddElevator", "evenElevator"],
+  },
+  {
+    type: "number",
+    name: "floor",
+    message: "몇층으로 이동하시겠습니까?",
+  },
+  {
+    type: "confirm",
+    name: "confirm",
+    message: "해당 층으로 이동하시겠습니까?",
+  },
+];
+
+inquirer.prompt(promptReq).then((answers) => {
+  if (answers.elevator === "oddElevator") {
+    oddElevator(answers.floor);
+  } else {
+    evenElevator(answers.floor);
+  }
+});
