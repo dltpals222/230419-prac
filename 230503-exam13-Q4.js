49f73b269ee291dd41f416365c619a84e7d8d432
leesemin <leesemin222@gmail.com>
2023-05-04 01:48:55 +0900

    4번문제 완

--- 230503-exam13-Q4.js
+++ 230503-exam13-Q4.js
@@ -1,31 +1,64 @@
 function exampleOne(numberValue) {
   // sequence .1
   // 매개변수가 정수인지 확인하는 함수 integerCheck()
-  function integerCheck(data) {
-    if (Number.isInteger(data) === true) {
-      return data;
-    } else {
-      return new Error("이 함수는 정수가 필요합니다.");
+  class integerCheck {
+    constructor(data) {
+      this.data = data;
+    }
+    set data(value) {
+      if (Number.isInteger(value) !== true) {
+        throw new Error("이 함수는 정수가 필요합니다.");
+      }
+      this._data = value;
+    }
+
+    get check() {
+      return this._data;
     }
   }
+
   // sequence .2
-  function innerOne(one) {
-    return one + 1;
+  class innerOne extends integerCheck {
+    constructor(data) {
+      super(data);
+    }
+
+    get one() {
+      return super.check + 1;
+    }
   }
 
   // sequence .3
-  function innerTwo(two) {
-    return two + 2;
+  class innerTwo extends innerOne {
+    constructor(data) {
+      super(data);
+    }
+
+    get two() {
+      return super.one + 2;
+    }
   }
 
   // sequence .4
-  function innerThree(three) {
-    return three + 3;
+  class innerThree extends innerTwo {
+    constructor(data) {
+      super(data);
+    }
+
+    get three() {
+      return super.two + 3;
+    }
   }
 
   // sequence .5
-  function innerFour(four) {
-    return four + 4;
+  class innerFour extends innerThree {
+    constructor(data) {
+      super(data);
+    }
+
+    get four() {
+      return super.three + 4;
+    }
   }
 
   //sequence(시퀀스가) 1 -> 2 -> 3 -> 4 -> 5 형태로 실행되어야 합니다.
@@ -41,13 +74,14 @@ function exampleOne(numberValue) {
   //배운적 없음 : Q.4 부모 함수를 클래스로 만들고 인스턴스에 '동적'으로 4개의 inner 함수를 매서드로 추가할 수 있을까요?
 
   // ------호출부분 ------
-  integerCheck(numberValue);
-  const first = innerOne(numberValue);
-  const second = innerTwo(numberValue);
-  const third = innerThree(numberValue);
-  const fourth = innerFour(numberValue);
-  return fourth;
+
+  const first = new innerFour(numberValue);
+  return first;
 }
 
 const result = exampleOne(10);
-console.log(result);
+console.log(result.check);
+console.log(result.one);
+console.log(result.two);
+console.log(result.three);
+console.log(result.four);
